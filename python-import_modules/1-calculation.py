4c887310a12bd9d74fa70710bca229841b69ce09
Felipe Santos <felipecassiors@gmail.com>
2024-01-22 02:22:28 -0500

    2-args.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,7 +3,13 @@ from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
-print(f'{a} + {b} = {add(a, b)}')
-print(f'{a} - {b} = {sub(a, b)}')
-print(f'{a} * {b} = {mul(a, b)}')
-print(f'{a} / {b} = {div(a, b)}')
+
+def main():
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
+
+if __name__ == "__main__":
+    main()
+
