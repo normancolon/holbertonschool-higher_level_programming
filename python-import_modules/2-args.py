cc7aab5ac5fdcabd79c81991b2b8ea934dd246df
Felipe Santos <felipecassiors@gmail.com>
2024-01-22 02:19:44 -0500

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-import sys
+from calculator_1 import add, sub, mul, div
+
+def main():
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
 
 if __name__ == "__main__":
-    num_args = len(sys.argv) - 1
-    if num_args == 0:
-        print("0 arguments.")
-    elif num_args == 1:
-        print("1 argument:")
-    else:
-        print(f"{num_args} arguments:")
-
-    for i in range(1, len(sys.argv)):
-        print(f"{i}: {sys.argv[i]}")
+    main()
 
