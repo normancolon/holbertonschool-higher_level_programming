dbb1815887d4ea7beb8ecc7f4f37df763eb86ec4
Felipe Santos <felipecassiors@gmail.com>
2024-01-22 02:15:56 -0500

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 import sys
 
-if __name__ == '__main__':
-    n = len(sys.argv) - 1
-    print(f'{n} argument{'' if n == 1 else 's'}:')
-    for i, arg in enumerate(sys.argv[1:], 1):
-        print(f'{i}: {arg}')
+if __name__ == "__main__":
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print(f"{num_args} arguments:")
+
+    for i in range(1, len(sys.argv)):
+        print(f"{i}: {sys.argv[i]}")
+
