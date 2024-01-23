f0afa9cfc34b37d0048debf5e45fd46730c7d582
LAB <lab@kali>
2024-01-23 11:58:53 -0500

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,53 +1,11 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 
-def add(number1, number2):
-    """My addition function
-
-    Args:
-        number1: first integer
-        number2: second integer
-
-    Returns:
-        The return value. number1 + number2
-    """
-    return (number1 + number2)
-
-
-def sub(minuend, subtrahend):
-    """My subtraction function
-
-    Args:
-        minuend: first integer
-        subtrahend: second integer
-
-    Returns:
-        The return value. minuend - subtrahend
-    """
-    return (minuend - subtrahend)
-
-
-def mul(multiplicand, multiplier):
-    """My multiplication function
-
-    Args:
-        multiplicand: first integer
-        multiplier: second integer
-
-    Returns:
-        The return value. multiplicand * multiplier
-    """
-    return (multiplicand * multiplier)
-
-
-def div(dividend, divisor):
-    """My division function
-
-    Args:
-        dividend: first integer
-        divisor: second integer
-
-    Returns:
-        The return value. dividend / divisor
-    """
-    return int(dividend / divisor)
+if __name__ == "__main__":
+    x = 10
+    y = 5
+    print("{:d} + {:d} = {:d}".format(x, y, add(x, y)))
+    print("{:d} - {:d} = {:d}".format(x, y, sub(x, y)))
+    print("{:d} * {:d} = {:d}".format(x, y, mul(x, y)))
+    print("{:d} / {:d} = {:d}".format(x, y, div(x, y)))
 
