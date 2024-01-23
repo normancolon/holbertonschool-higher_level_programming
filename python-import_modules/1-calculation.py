b02c26b08c9695499fbc07ad3cef6a81af10988b
LAB <lab@kali>
2024-01-23 11:54:57 -0500

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,13 +1,53 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
 
-    from calculator_1 import add, sub, mul, div
+def add(number1, number2):
+    """My addition function
 
-    num1 = 15
-    num2 = 3
+    Args:
+        number1: first integer
+        number2: second integer
 
-    print("{:d} + {:d} = {:d}".format(num1, num2, add(num1, num2)))
-    print("{:d} - {:d} = {:d}".format(num1, num2, sub(num1, num2)))
-    print("{:d} * {:d} = {:d}".format(num1, num2, mul(num1, num2)))
-    print("{:d} / {:d} = {:d}".format(num1, num2, div(num1, num2)))
+    Returns:
+        The return value. number1 + number2
+    """
+    return (number1 + number2)
+
+
+def sub(minuend, subtrahend):
+    """My subtraction function
+
+    Args:
+        minuend: first integer
+        subtrahend: second integer
+
+    Returns:
+        The return value. minuend - subtrahend
+    """
+    return (minuend - subtrahend)
+
+
+def mul(multiplicand, multiplier):
+    """My multiplication function
+
+    Args:
+        multiplicand: first integer
+        multiplier: second integer
+
+    Returns:
+        The return value. multiplicand * multiplier
+    """
+    return (multiplicand * multiplier)
+
+
+def div(dividend, divisor):
+    """My division function
+
+    Args:
+        dividend: first integer
+        divisor: second integer
+
+    Returns:
+        The return value. dividend / divisor
+    """
+    return int(dividend / divisor)
 
