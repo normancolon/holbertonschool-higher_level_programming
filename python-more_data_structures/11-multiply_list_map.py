db8aee609a237b031b32d8c8bfb6d9661dc0c25a
Felipe Santos <felipecassiors@gmail.com>
2024-01-30 21:20:42 -0500

    0-square_matrix_simple.py

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
     return list(map(lambda x: x * number, my_list))
-# Test the function
+
 if __name__ == "__main__":
+    # Test the function
     my_list = [1, 2, 3, 4, 6]
     new_list = multiply_list_map(my_list, 4)
     print(new_list)
     print(my_list)
+
