081aeccb33c698d517436aaeae8a112b20c8266d
Felipe Santos <felipecassiors@gmail.com>
2024-02-08 14:23:55 -0500

    7-base_geometry.txt

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+"""
+This module defines the inherits_from function that checks
+if an object is an instance of a class that inherited (directly or
+indirectly) from the specified class, excluding objects that are
+direct instances of the specified class.
+"""
+
 def inherits_from(obj, a_class):
     """
-    Checks if the object is an instance of a class that inherited (directly or
-    indirectly) from the specified class.
+    Determine if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class.
 
     Args:
         obj: The object to check.
@@ -10,6 +17,6 @@ def inherits_from(obj, a_class):
 
     Returns:
         True if obj is an instance of a class that inherited from a_class,
-        False otherwise.
+        excluding direct instances of a_class. False otherwise.
     """
     return isinstance(obj, a_class) and type(obj) != a_class
