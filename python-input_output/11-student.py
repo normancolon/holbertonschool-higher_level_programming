61aeef67fdd87281c9aef2b976056f3399b9143a
LAB <lab@kali>
2024-02-09 14:17:38 -0500

    5-save_to_json_file.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,43 +1,23 @@
 #!/usr/bin/python3
 
-"""Defines a class Learner for representing participants of online courses."""
+"""Defines the Student class."""
 
-class Learner:
-    """Represents an individual enrolled in online courses."""
 
-    def __init__(self, first_name, last_name, age, enrolled_courses=None):
-        """Initializes a new Learner instance.
+class Student:
+    """Represents a student."""
 
-        Parameters:
-            first_name (str): The learner's first name.
-            last_name (str): The learner's last name.
-            age (int): The learner's age.
-            enrolled_courses (list): A list of courses the learner is enrolled in.
-        """
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student instance."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        self.enrolled_courses = enrolled_courses if enrolled_courses is not None else []
 
-    def add_course(self, course_name):
-        """Adds a new course to the learner's list of enrolled courses.
+    def to_json(self):
+        """Retrieve a dictionary representation of a Student instance."""
+        return self.__dict__
 
-        Parameters:
-            course_name (str): The name of the course to add.
-        """
-        if course_name not in self.enrolled_courses:
-            self.enrolled_courses.append(course_name)
-
-    def profile_summary(self):
-        """Generates a summary of the learner's profile.
-
-        Returns:
-            dict: A dictionary containing the learner's details and enrolled courses.
-        """
-        return {
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "age": self.age,
-            "enrolled_courses": self.enrolled_courses
-        }
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance."""
+        for key, value in json.items():
+            setattr(self, key, value)
 
