7a81465a84459ccb53c5a8b6e15c2afabd3e8a16
LAB <lab@kali>
2024-02-09 14:23:48 -0500

    12-pascal_triangle.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,41 +1,26 @@
 #!/usr/bin/python3
+"""Defines the Student class."""
 
-"""Defines a class Learner for representing participants of online courses."""
 
-class Learner:
-    """Represents an individual enrolled in online courses."""
+class Student:
+    """A student."""
 
-    def __init__(self, first_name, last_name, age, enrolled_courses=None):
-        """Initializes a new Learner instance.
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
 
-        Parameters:
-            first_name (str): The learner's first name.
-            last_name (str): The learner's last name.
-            age (int): The learner's age.
-            enrolled_courses (list): A list of courses the learner is enrolled in.
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        self.enrolled_courses = enrolled_courses if enrolled_courses is not None else []
 
-    def add_course(self, course_name):
-        """Adds a new course to the learner's list of enrolled courses.
-
-        Parameters:
-            course_name (str): The name of the course to add.
-        """
-        self.enrolled_courses.append(course_name)
-
-    def profile_summary(self):
-        """Generates a summary of the learner's profile.
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance.
 
         Returns:
-            dict: A dictionary containing the learner's details and enrolled courses.
+            dict: A dictionary containing all keys/values of __dict__ of the instance.
         """
-        return {
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "age": self.age,
-            "enrolled_courses": self.enrolled_courses
-        }
+        return self.__dict__
