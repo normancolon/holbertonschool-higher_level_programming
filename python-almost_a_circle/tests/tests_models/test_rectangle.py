f74331e4c31a219375642e4279659c140c4f4744
Felipe Santos <felipecassiors@gmail.com>
2024-02-17 11:13:53 -0500

    Square.json

--- python-almost_a_circle/tests/tests_models/test_rectangle.py
+++ python-almost_a_circle/tests/tests_models/test_rectangle.py
@@ -133,6 +133,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r1.id, 89)
         r1.update(width=1)
         self.assertEqual(r1.width, 1)
+        
         # Continue for height, x, y as per the provided requirements
     def test_rectangle_save_to_file_none(self):
         """Test of Rectangle.save_to_file(None)"""
@@ -153,6 +154,5 @@ class TestRectangle(unittest.TestCase):
         with open("Rectangle.json", "r") as file:
             self.assertIn('"width": 1', file.read())
             self.assertIn('"height": 2', file.read())
-            
 if __name__ == '__main__':
     unittest.main()
