fa3458d19a7737364e9a2f2be7e230d009b327e9
Felipe Santos <felipecassiors@gmail.com>
2024-02-17 09:31:16 -0500

    README.md

--- python-almost_a_circle/tests/tests_models/test_rectangle.py
+++ python-almost_a_circle/tests/tests_models/test_rectangle.py
@@ -49,12 +49,51 @@ class TestBase(unittest.TestCase):
 class TestRectangle(unittest.TestCase):
     """Test cases for the Rectangle class."""
 
+    def setUp(self):
+        """Set up for test cases"""
+        Base._Base__nb_objects = 0
+
+    def test_rectangle(self):
+        """Test case for non-list arguments"""
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r1.width, 10)
+        self.assertEqual(r1.height, 2)
+        r3 = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(r3.id, 5)
+        self.assertEqual(r3.width, 1)
+        self.assertEqual(r3.height, 2)
+        self.assertEqual(r3.x, 3)
+        self.assertEqual(r3.y, 4)
+
+    def test_rectangle_with_negative_arguments(self):
+        """Test case for negative arguments"""
+        self.assertRaises(ValueError, Rectangle, 10, -2)
+        self.assertRaises(ValueError, Rectangle, -10, 2)
+        self.assertRaises(ValueError, Rectangle, 10, 2, -3)
+        self.assertRaises(ValueError, Rectangle, 10, 2, 3, -4)
+
+    def test_rectangle_with_zero_arguments(self):
+        """Test case for zero arguments"""
+        self.assertRaises(ValueError, Rectangle, 10, 0)
+        self.assertRaises(ValueError, Rectangle, 0, 10)
+
+    def test_rectangle_with_non_list_argument(self):
+        """Test case for non-list arguments"""
+        self.assertRaises(TypeError, Rectangle, 2, "10")
+        self.assertRaises(TypeError, Rectangle, "10", 2)
+        self.assertRaises(TypeError, Rectangle, 10, 2, "5")
+        self.assertRaises(TypeError, Rectangle, 10, 2, 5, "3")
+
     def test_area(self):
         """Test case for area method"""
         r1 = Rectangle(1, 2)
-        self.assertEqual(r1.area(), 2, "The area method did not return the correct result.")
+        self.assertEqual(r1.area(), 2)
 
-# Add more Rectangle tests here as needed
+    def test_str_rectangle(self):
+        """Test case for __str__ method"""
+        r1 = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(str(r1), "[Rectangle] (5) 3/4 - 1/2")
 
 if __name__ == '__main__':
     unittest.main()
