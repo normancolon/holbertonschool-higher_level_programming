52c68ce596ccc55e5d0688686c26d069597cb293
Felipe Santos <felipecassiors@gmail.com>
2024-03-06 18:24:15 -0500

    2-create_read_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,10 @@
--- Script that creates an user in MySQL server
--- Query to create the user 'user_0d_1' in MySQL server
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+-- Generate table with mandatory ID
+-- Ensures 'id_not_null' table's creation on MySQL
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT NOT NULL DEFAULT 1,
+    name VARCHAR(256)
+);
+-- Display MySQL users' rights
+-- Enumerates 'user_0d_1' and 'user_0d_2' privileges
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,13 +1,6 @@
--- Script that lists genres along with the count of associated TV shows
-SELECT 
-    tv_genres.name AS genre, 
-    COUNT(tv_show_genres.show_id) AS number_of_shows -- Counts shows per genre
-FROM 
-    tv_show_genres
-JOIN 
-    tv_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY 
-    genre
-ORDER BY 
-    number_of_shows DESC, 
-    tv_genres.id ASC;
+-- Counts shows per genre, sorted by count and genre ID
+SELECT g.name AS genre, COUNT(s.genre_id) AS number_of_shows
+FROM tv_show_genres s
+JOIN tv_genres g ON g.id = s.genre_id
+GROUP BY s.genre_id
+ORDER BY number_of_shows DESC, g.id ASC;
